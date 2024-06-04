2a2ff4c06b9307678b724a35c44955545b58c575
Seth Franklin <sethfranklin@Seths-MacBook-Air.local>
2024-06-03 21:52:11 -0400

    Changed the naming convention of databases

--- data-stores/mysql/main.tf
+++ data-stores/mysql/main.tf
@@ -5,7 +5,7 @@ resource "aws_db_instance" "example" {
   allocated_storage   = 10
   instance_class      = "db.t3.micro"
   skip_final_snapshot = true
-  db_name             = var.db_name
+  db_name             = "$newversion{var.db_name}"
 
   username = var.db_username
   password = var.db_password
