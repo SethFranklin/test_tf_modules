95a62be9773ee79f3ae611e742990cf03f5fb5a6
Seth Franklin <sethfranklin@Seths-MacBook-Air.local>
2024-06-03 21:35:39 -0400

    Initial commit of modules repo

---
+++ services/webserver-cluster/variables.tf
@@ -0,0 +1,41 @@
+
+# Required variables
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+  type        = string
+}
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket for the database's remote state"
+  type        = string
+}
+
+variable "db_remote_state_key" {
+  description = "The path for the database's remote state in the S3 bucket"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instances to run (e.g. t3.micro)"
+  type        = string
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 instances in the ASG"
+  type        = number
+}
+
+variable "max_size" {
+  description = "The maximum number of EC2 instances in the ASG"
+  type        = number
+}
+
+# Optional variables
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
